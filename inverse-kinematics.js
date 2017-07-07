a3b578fae56be8681eac163f91594b33dfef750f
Ryo Suzuki <ryosuzk@gmail.com>
2017-07-06 20:10:13 -0600

    Draw rectangle with points

--- inverse-kinematics.js
+++ inverse-kinematics.js
@@ -30,14 +30,11 @@ function solve(pos, l1, l2) {
   return [theta1, theta2]
 }
 
-function draw() {
-  background(255)
-
-  let l1 = 200
-  let l2 = 200
-  let pos = { x: mouseX, y: mouseY }
+function animate(pos) {
+  fill(200)
+  let l1 = 300
+  let l2 = 300
   let ans = solve(pos, l1, l2)
-
   let theta1 = ans[0]
   let theta2 = ans[1]
 
@@ -53,8 +50,49 @@ function draw() {
 
   let edge1 = Edge(p1, theta1, l1)
   let edge3 = Edge(p3, theta1, l1)
-
   let edge2 = Edge(p2, theta1+theta2, l2)
   let edge4 = Edge(p1, theta1+theta2, l2)
 
 }
+
+let points = [
+  { x: 400, y: 100 },
+  { x: 800, y: 100 },
+  { x: 800, y: 500 },
+  { x: 400, y: 500 },
+]
+
+let i = 0
+let j = 0
+function drawLine(p0, p1) {
+  let pos = {
+    x: (p0.x*(100-i) + p1.x*i)/100,
+    y: (p0.y*(100-i) + p1.y*i)/100,
+  }
+  animate(pos)
+  if (i < 100) {
+    i++
+  } else {
+    i = 0
+    j++
+  }
+}
+
+function drawRect() {
+
+}
+
+function draw() {
+  background(255)
+
+  for (let i=0; i<points.length; i++) {
+    let p0 = points[i]
+    let p1 = points[(i+1)%points.length]
+    line(p0.x, p0.y, p1.x, p1.y)
+  }
+  drawLine(points[j%4], points[(j+1)%4])
+}
+
+
+
+
